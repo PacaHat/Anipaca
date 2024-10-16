fb7a9834761162eabaa24b8b4edde265cd8704b8
Raisul Islam <118072581+PacaHat@users.noreply.github.com>
2024-10-17 02:22:17 +0600

    Add files via upload

---
+++ _config.php
@@ -0,0 +1,28 @@
+<?php
+
+$websiteTitle = "AniPaca"; // Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = $websiteUrl . "/files/images/logo.png?v=2"; // Logo
+$contactEmail = "animemelon69@gmail.com"; // Contact Email
+
+$version = "0.3";
+
+//Donate 
+$donate = "#";
+
+// Socials 
+$telegram = "https://t.me/#"; // telegram
+$discord = "https://discord.gg/aVvqx77RGs"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+
+
+
+$disqus = "https://indianime.disqus.com"; // Disqus
+
+
+// API URL
+$api = "https://pxr15-e5eo-m8j26bszy-animemelon69-gmailcoms-projects.vercel.app"; // 
+
+
+$banner = $websiteUrl . "/files/images/banner.png";  //Banner
